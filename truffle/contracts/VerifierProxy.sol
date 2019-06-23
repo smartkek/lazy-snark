e3c694f7765c776120903978635970fc2ccb60c0
Evgeny Marchenko <marchenko@smartdec.ru>
2019-06-23 16:15:25 +0300

    fix assert bug

--- truffle/contracts/VerifierProxy.sol
+++ truffle/contracts/VerifierProxy.sol
@@ -15,10 +15,9 @@ contract VerifierProxy is IVerifier {
     // Truffle gives `UnimplementedFeatureError: Encoding struct from calldata is not yet supported.`
     // that's why function is public and uses memory location modifier 
     function isValid(Data memory data, Proof memory proof) public returns (bool) {
-        // bytes memory payload = abi.encodeWithSelector(verifier.verifyTx.selector, proof, data);
-        // (bool success, bytes memory r) = address(verifier).call(payload);
-        // require(success);
-        // return abi.decode(r, (bool));
-        return verifier.verifyTx(proof.a, proof.b, proof.c, data.input);
+        bytes memory payload = abi.encodeWithSelector(verifier.verifyTx.selector, proof.a, proof.b, proof.c, data.input);
+        (bool success, bytes memory r) = address(verifier).call(payload);
+        return success && abi.decode(r, (bool));
+        // return verifier.verifyTx(proof.a, proof.b, proof.c, data.input);
     }
 }
