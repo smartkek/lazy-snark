c1bf14774ebf8b2c5ab3035fa43fbedb70b79da5
Drygin <drygin@smartdec.ru>
2019-07-03 19:59:16 +0300

    wip: remove beta_g1 and delta_g1

--- test_back_fluence/src/main.rs
+++ test_back_fluence/src/main.rs
@@ -72,9 +72,9 @@ fn main() {
     //vk.alpha_g1
     let mut hex_string = hex::decode("1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd").unwrap();
     hex_string.append(hex::decode("006653d8d2e65ab55fa795c44971eabcc6dbb1dd383c7a8a20de68486eb28154").unwrap().as_mut());
-    //vk.beta_g1
-    hex_string.append(hex::decode("09dcdb7f3d5d8f36be6363578bc80c86bb0978b2da7f396e366552c19327d2fa").unwrap().as_mut());
-    hex_string.append(hex::decode("06c8435a1215747694d059d9b1e4a36d6acd204af1efaf96c22fa3c978b7b57e").unwrap().as_mut());
+    //vk.beta_g1 = vk.alpha_g1 - ZoKrates does not return that and it is not neaded for verification
+    hex_string.append(hex::decode("1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd").unwrap().as_mut());
+    hex_string.append(hex::decode("006653d8d2e65ab55fa795c44971eabcc6dbb1dd383c7a8a20de68486eb28154").unwrap().as_mut());
     //vk.beta_g2
     hex_string.append(hex::decode("1fb73b30d15c6acbf8e723365798381cddccd353d3473853f8586045da20d9c7").unwrap().as_mut());
     hex_string.append(hex::decode("0ea0d501ac34e65153ff483d15864b7b99d03727e3d18a10442ed2c5b7475616").unwrap().as_mut());
@@ -85,9 +85,9 @@ fn main() {
     hex_string.append(hex::decode("257c137fe6037e6302bcc0e0c8d12c55c32663ea30aaf8602cb505cce116934d").unwrap().as_mut());
     hex_string.append(hex::decode("0b9641484e773a5c30f604b912e7008d8c80221eb8af076206d1cb62d5459d6d").unwrap().as_mut());
     hex_string.append(hex::decode("1422be63e21d8de851ba9ef345b3ba8e9f30dcc9b2b9181700c6c7387fd0000c").unwrap().as_mut());
-    //vk.delta_g1
-    hex_string.append(hex::decode("2685638d1ab1c53e240faa4c600e08bf5acaeb923f530d6c6b02d752c4401c6e").unwrap().as_mut());
-    hex_string.append(hex::decode("16bd94558a5419e22b4fca334f8e11c67d2aeaa0668521a6c41cd5804c03afa3").unwrap().as_mut());
+    //vk.delta_g1 = vk.alpha_g1 - ZoKrates does not return that and it is not neaded for verification
+    hex_string.append(hex::decode("1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd").unwrap().as_mut());
+    hex_string.append(hex::decode("006653d8d2e65ab55fa795c44971eabcc6dbb1dd383c7a8a20de68486eb28154").unwrap().as_mut());
     //vk.delta_g2
     hex_string.append(hex::decode("199f02185af5ab7a2646ad6523cf0e75160919770fac8be6d87a2c85f5c62fa2").unwrap().as_mut());
     hex_string.append(hex::decode("2415e319be12366f2b5f59f6475008d3fa8f2486dbbd417ff979dc727ac8b506").unwrap().as_mut());
@@ -117,14 +117,15 @@ fn main() {
     /*let proof_file = File::open(PathBuf::from("./proof.bin")).unwrap();
     let proof : Proof<Bn256> = Proof::read(proof_file).unwrap();*/
 
+    // a
     let mut hex_string = hex::decode("143c347b098006a40a22eaa2d2bc56222e734f55bd717721c90886518872015d").unwrap();
     hex_string.append(hex::decode("10f7e2ca628844cb7c2ccacc8abb4930cd226dcd37f9e83860f942a177020c7d").unwrap().as_mut());
-    
+    // b
     hex_string.append(hex::decode("2902bfe7dba8187bd73795211f2eece7d5aa3ba8a1f27e34c8ba148cad4ac743").unwrap().as_mut());
     hex_string.append(hex::decode("2e0b0d4278e6b20f5b2e31fa425c7c91d1eebdaeb01fef228b5954e2a0c01f8a").unwrap().as_mut());
     hex_string.append(hex::decode("2f2c29c73d45cfedf4334873e418860f561c3d7fa4090fe7aeb686023398874d").unwrap().as_mut());
     hex_string.append(hex::decode("14f4411f6edfd09d31ad7f6f78dbbe7debfa66a0cb4e0450888790b4293eeaf1").unwrap().as_mut());
-
+    // c
     hex_string.append(hex::decode("125c6452b97d9b83600e903313aa93284a8b8d8318050ed6af65c10df7ebb5ec").unwrap().as_mut());
     hex_string.append(hex::decode("1a5702d896bdf72c8853dc91819a5de30d7415c61c0eb9c55d947485b268ef89").unwrap().as_mut());
 
