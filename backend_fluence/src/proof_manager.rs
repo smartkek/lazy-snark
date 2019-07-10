8b8f14a7e9d660361178f355d67a4c62dad0339d
Drygin <drygin@smartdec.ru>
2019-07-10 19:09:30 +0300

    fix line break issue

--- backend_fluence/src/proof_manager.rs
+++ backend_fluence/src/proof_manager.rs
@@ -25,45 +25,45 @@ pub struct ProofManager {
 impl ProofManager {
     pub fn new() -> Self {
         let vk_alpha_g1 = hex::decode(
-            "2e0a814dd75e4118233ddf6a916a813c40bae07d976fdcd01dbfa22bea641a96
+            "2e0a814dd75e4118233ddf6a916a813c40bae07d976fdcd01dbfa22bea641a96\
             1779e77cff5e54cf2cdc237e51cd6d95ef2c37ab6a7d5f9ce0a242188e1a1fe3",
         )
         .unwrap();
 
         //some g1 point, because ZoKrates does not return that and it is not needed for verification
         let vk_beta_g1 = hex::decode(
-            "1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd
+            "1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd\
             006653d8d2e65ab55fa795c44971eabcc6dbb1dd383c7a8a20de68486eb28154",
         )
         .unwrap();
 
         let vk_beta_g2 = hex::decode(
-            "021548b93199574bdef2be8cb1908a1079b1664d8a041d2e297c3aa6c554855c
-            190b2d5d03854400e2c2a702f502813677a1d4be920d79648f810e320a30f2c5
-            0bc956fa715451d64e20b260759c2ae74a82b68f1eef86504051cd3ae547f282
+            "021548b93199574bdef2be8cb1908a1079b1664d8a041d2e297c3aa6c554855c\
+            190b2d5d03854400e2c2a702f502813677a1d4be920d79648f810e320a30f2c5\
+            0bc956fa715451d64e20b260759c2ae74a82b68f1eef86504051cd3ae547f282\
             011192ee83c0347e363b7c5fffe156fbadd91591b35dc8fe912d2b498c3a9301",
         )
         .unwrap();
 
         let vk_gamma_g2 = hex::decode(
-            "1c4c46720835faf06e35cd85f05c589a1a98f58112ecf7aacf0deac60681f5a4
-            1b438f01daf6402ff298981b74f80a5e79c39cce21c67770f74b89e65eb3b9ca
-            101b8c9c29aa1ac1a709878f6eb4d4a74f4ed1368a18f29c2762b76b8c389f4d
+            "1c4c46720835faf06e35cd85f05c589a1a98f58112ecf7aacf0deac60681f5a4\
+            1b438f01daf6402ff298981b74f80a5e79c39cce21c67770f74b89e65eb3b9ca\
+            101b8c9c29aa1ac1a709878f6eb4d4a74f4ed1368a18f29c2762b76b8c389f4d\
             009538b3640e10082d0bf4b18b997fef6af2e7cceb942ebb26bd263e8805fedd",
         )
         .unwrap();
 
         //some g1 point, because ZoKrates does not return that and it is not needed for verification
         let vk_delta_g1 = hex::decode(
-            "1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd
+            "1fea09defec64586a976a33dbfb70961fc7e03fb6f4d5a1e074f97312ce789cd\
             006653d8d2e65ab55fa795c44971eabcc6dbb1dd383c7a8a20de68486eb28154",
         )
         .unwrap();
 
         let vk_delta_g2 = hex::decode(
-            "25161a4cc549ffabd2c4508038c12d49447c15e9c565b025183ff6114ffcc58b
-            110f2b773f6d9632162bc2c629467a58e7539ed0f0dc64ff4fd8f63baf4b5a32
-            0eb80be9e5a3f3f4cb0e39edc1db88dbf8de59b0c800b72dcc34d9c0fae14d55
+            "25161a4cc549ffabd2c4508038c12d49447c15e9c565b025183ff6114ffcc58b\
+            110f2b773f6d9632162bc2c629467a58e7539ed0f0dc64ff4fd8f63baf4b5a32\
+            0eb80be9e5a3f3f4cb0e39edc1db88dbf8de59b0c800b72dcc34d9c0fae14d55\
             0839d69bfc27640a59af741138d4f34500d925eb1a4e9fd57fcda269a7411c33",
         )
         .unwrap();
@@ -71,17 +71,17 @@ impl ProofManager {
         let vk_ic_len = 6;
 
         let vk_ic = hex::decode(
-            "2bb604557c5f1096973ab8afe980ea3ae23bd7457f3f11f67fb395f2d1f3b568
-            0f12fdb646ea572637ea6e1bbf04158bcabe6947cf614c67efb3f0278279f866
-            228bbefb9d7457c97766bcae9412c6ddd1de8e3dbcf1606ca6b8f027836affee
-            01bf2712a663f5a72a469ea83a4c3d453c6023a0cd5d5f86330157f1505d62b3
-            23af3409b4b3fb3f194dc683be70c5e442de55544edeace8f891a891a4701ca3
-            1d13edb38da07247e70158557cfa93097d90d92b9a2c99f190c1413f3fdf8828
-            00572fbfedfe16fd1dcae266bf009907451cd8db485325ad322fb658cb0c30ff
-            25415b150b181b2cbecc6f84382b0bd8fd49f2cf498da1c775ad624e5e7b7eaf
-            1a294f13fbf284a6e11c2f54ed2946fc5fd732dafbf49ac01ce741f224b57c29
-            182d4a788849c87d27548cbe3a511a0237cb0d4595425eee878d78c4eb4e5529
-            10ec12d1090de44b1aecb41030d123df2d61318c1928d6de10f916c9bfc2f681
+            "2bb604557c5f1096973ab8afe980ea3ae23bd7457f3f11f67fb395f2d1f3b568\
+            0f12fdb646ea572637ea6e1bbf04158bcabe6947cf614c67efb3f0278279f866\
+            228bbefb9d7457c97766bcae9412c6ddd1de8e3dbcf1606ca6b8f027836affee\
+            01bf2712a663f5a72a469ea83a4c3d453c6023a0cd5d5f86330157f1505d62b3\
+            23af3409b4b3fb3f194dc683be70c5e442de55544edeace8f891a891a4701ca3\
+            1d13edb38da07247e70158557cfa93097d90d92b9a2c99f190c1413f3fdf8828\
+            00572fbfedfe16fd1dcae266bf009907451cd8db485325ad322fb658cb0c30ff\
+            25415b150b181b2cbecc6f84382b0bd8fd49f2cf498da1c775ad624e5e7b7eaf\
+            1a294f13fbf284a6e11c2f54ed2946fc5fd732dafbf49ac01ce741f224b57c29\
+            182d4a788849c87d27548cbe3a511a0237cb0d4595425eee878d78c4eb4e5529\
+            10ec12d1090de44b1aecb41030d123df2d61318c1928d6de10f916c9bfc2f681\
             0621a1ea9bbbfa893358dfaa206ba1cb8af2ecca483c3c36f2a0c302da401c8f",
         )
         .unwrap();
